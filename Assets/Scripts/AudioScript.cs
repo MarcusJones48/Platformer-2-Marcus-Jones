84d03edafefc74ad14f2699c46895a0fb05e3aa9
Marcus Jones <marcusjones42@knights.ucf.edu>
2019-03-10 22:38:39 -0400

    Adding to my Project

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip MusicClipOne;
+    public AudioClip MusicClipTwo;
+    public AudioSource MusicSource;
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MusicSource.clip = MusicClipOne;
+            MusicSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            MusicSource.Stop();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            MusicSource.clip = MusicClipTwo;
+            MusicSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            MusicSource.Stop();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            MusicSource.loop = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            MusicSource.loop = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+    public float speed;
+    public float jumpForce;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        SetCountText();
+        SetLivesText();
+    }
+
+    
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      if (other.gameObject.CompareTag ("PickUp"))
+      {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText();
+            if (count == 4) 
+            {
+               transform.position = new Vector3(25.0f, transform.position.y,4.0f); 
+            }
+      }
+      else if (other.gameObject.CompareTag ("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            lives = lives - 1;
+            SetCountText();
+            SetLivesText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+        }
+
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            winText.text = "You Lose!";
+        }
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+       if(collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+}
